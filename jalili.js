e4e2d435c175f85c1ce43317c5742917692e64b6
Jimmie Lovell <jimmieomlovell@gmail.com>
2019-06-25 07:10:23 +0300

    Fixed the bug causing Jalili object not to be returned

--- jalili.js
+++ jalili.js
@@ -4,30 +4,32 @@
 
 	var version = "1.0";
 	var doc = global.document;
-	var toString = Object.toString;
+	var toString = Object.prototype.toString;
 
-	var Jalili = function(selector) {
-		return new Jalili._proto_.boot(selector);
+	function Jalili(selector) {
+		return init();
 	}
 
-	//Define properties and methods for internal use only
-	Jalili._proto_ = Jalili.prototype = {
+	Jalili.prototype = {
 		version : version,
 
 		constructor : Jalili,
 
 		length : 0,
+	}
 
-		boot : function(selector) {
-			selector = selector;
+	function init() {
+		function f(){}
+		f.prototype = Jalili;
 
-			return Jalili;
-		}
+		return new f();
 	}
 
 	//Copy the properties of p to o, and return o.
 	//If o and p have a property by the same name, o's property is overwritten.
-	Jalili.extend = function(o, p) {
+	Jalili.extend = function(p, o) {
+		o = o || this;
+
 		for(var prop in p) {
 			o[prop] = p[prop];
 		}
@@ -35,7 +37,7 @@
 		return o;
 	}
 
-	Jalili.extend(global, {
+	Jalili.extend({
 		isArray : function(a) {
 	  	return toString.call(a) === '[object Array]' || toString.call(a) === '[object NodeList]';
 		},
@@ -67,69 +69,71 @@
 		isWindow : function(o) {
 			return !isNull(o) && o === o.global;
 		}
-	});
+	}, global);
 
-	//Copy the properties of p to o, and return o.
-	//If o and p have a property by the same name, o's property is used.
-	Jalili.merge = function(o, p) {
-		for(var prop in p) {
-			if (o.hasOwnProperty[prop]) {
-				continue;
+	Jalili.extend({
+		//Copy the properties of p to o, and return o.
+		//If o and p have a property by the same name, o's property is used.
+		merge : function(o, p) {
+			for(var prop in p) {
+				if (o.hasOwnProperty[prop]) {
+					continue;
+				}
+
+				o[prop] = p[prop];
 			}
 
-			o[prop] = p[prop];
-		}
+			return o;
+		},
 
-		return o;
-	}
+		//Remove properties from o if there is not a property with the same name in p.
+		restrict : function(o, p) {
+			for(var prop in o) {
+				if (!(prop in p)) {
+					delete o[prop];
+				}
+			}
 
-	//Remove properties from o if there is not a property with the same name in p.
-	Jalili.restrict = function(o, p) {
-		for(var prop in o) {
-			if (!(prop in p)) {
+			return o;
+		},
+
+		//For each property of p, delete a property with the same name from o.
+		subtract : function(o, p) {
+			for(var prop in p) {
 				delete o[prop];
 			}
-		}
 
-		return o;
-	}
+			return o;
+		},
 
-	//For each property of p, delete a property with the same name from o.
-	Jalili.subtract = function(o, p) {
-		for(var prop in p) {
-			delete o[prop];
-		}
+		//Return a new object holding properties of both o and p.
+		//If o and p have properties by the same name, values from o are used.
+		union : function(o, p) {
+			return this.extend(this.extend({},o), p);
+		},
 
-		return o;
-	}
+		//Return a new object that holds only properties of o also appearing in p
+		intersection : function(o, p) {
+			return this.restrict(this.extend({}, o), p);
+		},
 
-	//Return a new object holding properties of both o and p.
-	//If o and p have properties by the same name, values from o are used.
-	Jalili.union = function(o, p) {
-		return this.extend(this.extend({},o), p);
-	}
-
-	//Return a new object that holds only properties of o also appearing in p
-	Jalili.intersection = function(o, p) {
-		return this.restrict(this.extend({}, o), p);
-	}
-
-	//Return an array containing names of own properties of o
-	Jalili.keys = function(o) {
-		if (!isObject(o)) {
-			throw TypeError();
-		}
-
-		var result = [];
-
-		for (var prop in o) {
-			if (o.hasOwnProperty(prop)) {
-				result.push(prop);
+		//Return an array containing names of own properties of o
+		keys : function(o) {
+			if (!isObject(o)) {
+				throw TypeError();
 			}
-		}
 
-		return result;
-	}
+			var result = [];
+
+			for (var prop in o) {
+				if (o.hasOwnProperty(prop)) {
+					result.push(prop);
+				}
+			}
+
+			return result;
+		}
+	});
 
 	//freeze objects to make its properties non-configurable
 	Jalili.freeze = function(o) {
@@ -137,11 +141,23 @@
 			Object.freeze(o);
 		}
 	}
+
+	//inherit jalili object
+	Jalili.inherit = function() {
+		if (Object.create) {
+			return Object.create(this);
+		}
+
+		function f(){}
+		f.prototype = this;
+
+		return new f();
+	}
 	
-	Jalili.version = version;
 	Object.freeze(Jalili);
 
 	global.$ = global.Jalili = Jalili;
 
 	return Jalili;
+	
 })(window);
