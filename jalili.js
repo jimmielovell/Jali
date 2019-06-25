6642d68020bb894abf137a9298263a6de20300ef
Jimmie Lovell <jimmieomlovell@gmail.com>
2019-06-25 13:35:39 +0300

    Added array functionalities to the Jalili object

--- jalili.js
+++ jalili.js
@@ -7,7 +7,7 @@
 	var toString = Object.prototype.toString;
 
 	function Jalili(selector) {
-		return init();
+		return init(selector);
 	}
 
 	Jalili.prototype = {
@@ -18,11 +18,28 @@
 		length : 0,
 	}
 
-	function init() {
-		function f(){}
-		f.prototype = Jalili;
+	function init(selector) {
+		function F(){}
+		F.prototype = Jalili;
+		var jalili = new F();
 
-		return new f();
+		if (!isUndefined(selector) || !isNull(selector)) {
+			if (isString(selector)) {
+				var nodes =  doc.querySelectorAll(selector);
+
+				if (!empty(nodes)) {
+					jalili[0] = nodes.length == 1? nodes[0] : nodes;
+
+					return jalili[0];
+				}
+			} else if (isFunction(selector)) {
+				selector();
+			} else if (isArray(selector) || isObject(selector)) {
+				jalili[0] = selector;
+			}
+		}
+
+		return jalili;
 	}
 
 	//Copy the properties of p to o, and return o.
@@ -68,13 +85,23 @@
 
 		isWindow : function(o) {
 			return !isNull(o) && o === o.global;
+		},
+
+		empty : function(o) {
+			return o.length == 0;
+		},
+
+		each : function(arr, c) {
+			Array.prototype.forEach.call(this, c);
 		}
 	}, global);
 
 	Jalili.extend({
 		//Copy the properties of p to o, and return o.
 		//If o and p have a property by the same name, o's property is used.
-		merge : function(o, p) {
+		merge : function(p, o) {
+			o = o || this;
+			
 			for(var prop in p) {
 				if (o.hasOwnProperty[prop]) {
 					continue;
@@ -108,12 +135,16 @@
 
 		//Return a new object holding properties of both o and p.
 		//If o and p have properties by the same name, values from o are used.
-		union : function(o, p) {
+		union : function(p, o) {
+			o = o || this;
+
 			return this.extend(this.extend({},o), p);
 		},
 
 		//Return a new object that holds only properties of o also appearing in p
-		intersection : function(o, p) {
+		intersection : function(p, o) {
+			o = o || this;
+
 			return this.restrict(this.extend({}, o), p);
 		},
 
@@ -135,6 +166,84 @@
 		}
 	});
 
+	Jalili.extend({
+		pop : function() {
+			if (isArray(this[0])) {
+				return this[0].pop();
+			}
+		},
+
+		push : function(x) {
+			if (isArray(this[0])) {
+				return this[0].push(x);
+			}
+		},
+
+		join : function() {
+			if (isArray(this[0])) {
+				return this[0].join();
+			}
+		},
+
+		shift : function() {
+			if (isArray(this[0])) {
+				return this[0].shift();
+			}
+		},
+
+		unshift : function(x) {
+			if (isArray(this[0])) {
+				return this[0].unshift(x);
+			}
+		},
+
+		replace : function(i, x) {
+			if (isArray(this[0])) {
+				var l = this[0].length;
+
+				if (Math.abs(i) >= l) {
+					throw "Index out of bounds.";
+				}
+
+				if (i < 0) {
+					i = l+i;
+				}
+
+				return this[0][i] = x;
+			}
+		},
+
+		splice : function(i, n) {
+			if (isArray(this[0])) {
+				var l = this[0].length;
+
+				if (Math.abs(i) >= l) {
+					throw "Index out of bounds.";
+				}
+
+				return this[0].splice(i, Math.abs(n));
+			}
+		},
+
+		delete : function(i) {
+			return this.splice(i, 1);
+		},
+
+		concat : function() {
+			if (!empty(arguments)) {
+				var arr = isArray(this[0])? this[0] : [];
+
+				for (var i = 0; i < arguments.length; i++) {
+					if (isArray(arguments[i])) {
+						arr = arr.concat(arguments[i]);
+					}
+				}
+
+				return arr;
+			}
+		}
+	});
+
 	//freeze objects to make its properties non-configurable
 	Jalili.freeze = function(o) {
 		if (isObject(o)) {
@@ -148,16 +257,16 @@
 			return Object.create(this);
 		}
 
-		function f(){}
-		f.prototype = this;
+		function F(){}
+		F.prototype = this;
 
-		return new f();
+		return new F();
 	}
 	
 	Object.freeze(Jalili);
 
 	global.$ = global.Jalili = Jalili;
+	Node.prototype.$ = $;
 
 	return Jalili;
-	
-})(window);
+})(undefined !== window? window : this);
